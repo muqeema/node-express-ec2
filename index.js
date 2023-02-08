84d51f01b48a6d2a6f234a4f883d307f3f26f140
muqeemahmad12 <muqeem.ahmad@doceree.com>
2023-02-08 16:10:11 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+import express from "express";
+
+const app = express()
+
+app.listen(5001, () => console.log('API runing on port 5001'))
+
+app.get('/', (req, res) => res.json('My api running perfectly'))
